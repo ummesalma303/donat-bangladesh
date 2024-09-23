e3b5c40472b95c31d838bb5624455443bdb7e5ee
ummesalma303 <ummesalma333999@gmail.com>
2024-09-23 12:37:21 +0600

    decrees balance and added confirmation modal

--- js/donation.js
+++ js/donation.js
@@ -1,7 +1,22 @@
 document.getElementById('donate-btn')
 .addEventListener('click', function () {
     const donateInput = getValidInput('donate-input');
-    const money = getTextFieldValue('money-btn');
-    const total = donateInput + money;
-    document.getElementById('money-btn').innerText = total;
+    if (!donateInput || isNaN(donateInput) || donateInput < 0) {
+        document.getElementById('my_modal_5').classList.add('hidden')
+        alert('invalid input');
+        return
+    }
+    const donateBalance = getTextFieldValue('donate-balance');
+    const totalDonate = donateInput + donateBalance;
+    document.getElementById('donate-balance').innerText = totalDonate;
+
+    // decrees balance
+    const balance = getTextFieldValue('balance');
+    const newBalance = balance - donateInput;
+    if (totalDonate > balance) {
+        document.getElementById('my_modal_5').classList.add('hidden')
+        alert('you have not enough money');
+        return
+    }
+    document.getElementById('balance').innerText = newBalance;
 })
